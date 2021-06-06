0c59dee30bfbb59eec2f6ff190af9b708c74e3a1
David Schippers <david.schip@posteo.de>
2021-06-06 22:53:59 +0200

    Blocks now got own class

---
+++ flappy_bird/blocks.py
@@ -0,0 +1,45 @@
+import pyglet
+from pyglet import shapes
+import random
+
+class block(shapes.Rectangle):
+    def __init__(self, x, y, width, height, color = (0, 153, 76)):
+        super(block, self).__init__(x=x, y=y, width=width, height=height, color=color)
+
+class blocks():
+    def __init__(self, count, BLOCK_DIST, BLOCK_WIDTH, Y_TILING, HOLE, y_scale, x_scale, startpoint):
+
+        self.blocks = list()
+
+        self.x_scale = x_scale
+        self.y_scale = y_scale
+        self.count = count
+        self.block_width = BLOCK_WIDTH
+        self.block_dist = BLOCK_DIST
+
+        for block_pair_count in range(count):
+            height = random.randint(2, 5)
+
+            for block_count in range(2):
+                block_pair = [
+                    block(x=(block_pair_count + BLOCK_DIST * block_pair_count + startpoint) * x_scale,
+                                     y=0, width= x_scale * BLOCK_WIDTH, height= y_scale * height),
+                    block(x=(block_pair_count + BLOCK_DIST * block_pair_count + startpoint) * x_scale, y= y_scale * (
+                        height+HOLE), width= x_scale * BLOCK_WIDTH, height= y_scale * (Y_TILING-height))
+                ]
+
+            self.blocks.append(block_pair)
+
+    def update(self, BLOCK_SPEED):
+        
+        for block_pair in self.blocks:
+            for block in block_pair:
+                if block.x < - self.block_width * self.x_scale:
+                    block.x = (self.count + self.block_dist *
+                               self.count) * self.x_scale
+                block.x -= BLOCK_SPEED * self.x_scale
+
+    def draw(self):
+        for block_pair in self.blocks:
+            for block in block_pair:
+                block.draw()
--- flappy_bird/window.py
+++ flappy_bird/window.py
@@ -2,6 +2,7 @@ import pyglet
 import random
 from pyglet import shapes
 from bird import bird
+from blocks import blocks
 
 Y_TILING = 10
 X_TILING = 16
@@ -24,50 +25,30 @@ class app(pyglet.window.Window):
 
         self.startpoint = X_TILING / 2 + 3
 
-        self.blocks = list()
-        self.create_blocks()
+        self.blocks = blocks(BLOCK_COUNT, BLOCK_DIST, BLOCK_WIDTH, Y_TILING, HOLE, self.y_scale, self.x_scale, self.startpoint)
 
         self.bird = bird(x=50, y=Y_TILING/2 * self.y_scale,
                          radius=BIRDSIZE, color=(50, 225, 30))
 
         pyglet.gl.glClearColor(255, 255, 255, 1.0)
-        pyglet.clock.schedule_interval(self.update_app, SPEED)
-
-    def create_blocks(self):
-        for block_pair_count in range(BLOCK_COUNT):
-            height = random.randint(2, 5)
-
-            for block_count in range(2):
-                block_pair = [
-                    shapes.Rectangle(x=(block_pair_count + BLOCK_DIST * block_pair_count + self.startpoint) * self.x_scale,
-                                     y=0, width=self.x_scale * BLOCK_WIDTH, height=self.y_scale * height, color=(0, 153, 76)),
-                    shapes.Rectangle(x=(block_pair_count + BLOCK_DIST * block_pair_count + self.startpoint) * self.x_scale, y=self.y_scale * (
-                        height+HOLE), width=self.x_scale * BLOCK_WIDTH, height=self.y_scale * (Y_TILING-height), color=(0, 153, 76))
-                ]
-
-            self.blocks.append(block_pair)
+        
 
     def update_app(self, timer):
         self.clear()
 
-        for block_pair in self.blocks:
-            for block in block_pair:
-                if block.x < - BLOCK_WIDTH * self.x_scale:
-                    block.x = (BLOCK_COUNT + BLOCK_DIST *
-                               BLOCK_COUNT) * self.x_scale
-                block.x -= BLOCK_SPEED * self.x_scale
+        self.blocks.update(BLOCK_SPEED)
 
         self.bird.move()
 
     def on_draw(self):
         self.clear()
 
-        for block_pair in self.blocks:
-            for block in block_pair:
-                block.draw()
+        self.blocks.draw()
 
         self.bird.draw()
 
     def on_key_press(self, symbol, modifiers):
         if symbol == pyglet.window.key.UP:
             self.bird.move_up()
+        elif symbol == pyglet.window.key.DOWN:
+            pyglet.clock.schedule_interval(self.update_app, SPEED)
