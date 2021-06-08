65b220073c2efa17ba8c9832dcfc07ed9d5b84c9
David Schippers <david.schip@posteo.de>
2021-06-08 17:00:10 +0200

    not functional

--- flappy_bird/NN.py
+++ flappy_bird/NN.py
@@ -3,10 +3,8 @@ import numpy as np
 class Neural_Net:
     def __init__(self, input_count, output_count):
         
-        self.hidden_layer = np.random.rand(input_count, output_count)
+        self.hidden_layer = np.random.rand(output_count, input_count)
 
     def output(self, input):
 
-        result = input * self.hidden_layer
-
-        print(result)
+        return input * self.hidden_layer
