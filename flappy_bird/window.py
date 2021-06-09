103aceb4c740a15f4128c9a2a26dc8b6aaabceb4
David Schippers <david.schip@posteo.de>
2021-06-09 13:35:23 +0200

    Add comments

--- flappy_bird/window.py
+++ flappy_bird/window.py
@@ -17,9 +17,20 @@ BIRDSIZE = 15
 JUMP_HIGHT = 9
 GRAVITY = 0.25
 
+
 class app(pyglet.window.Window):
 
     def __init__(self) -> None:
+        """
+        Create the flappy bird window with blocks and bird.
+
+        Args:
+            self (undefined):
+
+        Returns:
+            None
+
+        """
         super(app, self).__init__()
 
         self.y_scale = self.get_size()[0] / Y_TILING
@@ -30,6 +41,18 @@ class app(pyglet.window.Window):
         pyglet.gl.glClearColor(255, 255, 255, 1.0)
 
     def set_variables(self):
+        """
+        Set all the variables:
+        - Crate all blocks
+        - Create a / all birds
+        - Set the starting point of the blocks
+        - (WIP) Create a restart buttom, if the game ends
+        - A variable if the game has already started
+
+        Args:
+            self (undefined):
+
+        """
         self.startpoint = X_TILING / 2 + 3
 
         self.blocks = blocks(BLOCK_COUNT, BLOCK_DIST, BLOCK_WIDTH,
@@ -37,38 +60,86 @@ class app(pyglet.window.Window):
 
         self.bird = bird(x=50, y=Y_TILING/2 * self.y_scale, gravity=GRAVITY, jump_height=JUMP_HIGHT,
                          radius=BIRDSIZE)
-        
+
         self.started = False
 
-        self.restart_button = button(self.get_size()[1] / 2 ,self.get_size()[0] / 2, 600, 600)
+        # self.restart_button = button(self.get_size()[1] / 2 ,self.get_size()[0] / 2, 600, 600)
 
     def update_app(self, timer):
+        """
+        Update the app and all the objects in that app:
+        - The blocks
+        - The bird
+
+        Args:
+            self (undefined):
+            timer (undefined): The timer of the app.
+
+        TODO: Let the NN of the bird decide if it wants to jump or not.
+
+        """
         self.clear()
 
         self.blocks.update(BLOCK_SPEED)
 
         self.bird.update(self.get_size()[0])
 
-        if self.blocks.check_collision(self.bird.x, self.bird.y, self.bird.radius * 0.8): # Multiply with a factor so it feels better
+        # Multiply with a factor so it feels better
+        if self.blocks.check_collision(self.bird.x, self.bird.y, self.bird.radius * 0.8):
             self.pause()
 
-        self.bird.decide_NN(self.blocks.nearest_block_coordinates(self.bird.x))
+        # self.bird.decide_NN(self.blocks.nearest_block_coordinates(self.bird.x))
 
     def on_draw(self):
+        """
+        Draw all the objects in the app:
+        - The blocks
+        - The bird / birds
+
+        Args:
+            self (undefined):
+
+        """
         self.clear()
 
         self.blocks.draw()
         self.bird.draw()
-    
+
     def pause(self):
-        self.restart_button.draw()
+        """
+        Pause the game and:
+        - (WIP) Draw the restart buttom to restart the game.
+
+        Args:
+            self (undefined):
+
+        """
+        # self.restart_button.draw()
 
         pyglet.clock.unschedule(self.update_app)
 
     def restart(self):
+        """
+        (WIP) Restart the whole game.
+
+        Args:
+            self (undefined):
+
+        TODO: Implement a working function.
+
+        """
         self.set_variables()
-    
+
     def on_key_press(self, symbol, modifiers):
+        """
+        The function to check keyboard inputs and react to them.
+
+        Args:
+            self (undefined):
+            symbol (undefined):
+            modifiers (undefined):
+
+        """
         if symbol == pyglet.window.key.UP or symbol == pyglet.window.key.SPACE:
             if not self.started:
                 pyglet.clock.schedule_interval(self.update_app, SPEED)
