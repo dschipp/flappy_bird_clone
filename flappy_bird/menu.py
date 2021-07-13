5752e362a738c968a0de815ecd24a6952a71b10d
David Schippers <david.schip@posteo.de>
2021-07-13 14:09:35 +0200

    Defining future plans

---
+++ flappy_bird/menu.py
@@ -0,0 +1,14 @@
+"""
+A class for the main menu displayed when the game is started or stopped.
+Shows game instructions and has a option menu for to change the properties of the game.
+"""
+# TODO: In the Background show a blurred version of the game played by a very good and saved bird
+
+import pyglet
+
+class menu:
+    def __init__(self) -> None:
+        pass
+
+    def draw():
+        pass
