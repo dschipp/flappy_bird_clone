678cdeb1ba0f6748a6991751aee3bb7754c328e2
David Schippers <david.schip@posteo.de>
2022-02-27 20:34:06 +0100

    try and error

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,7 +12,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath(os.path.join('..','..','..', 'flappy_bird')))
+sys.path.insert(0, os.path.abspath(os.path.join('..','..', 'flappy_bird')))
 
 
 # -- Project information -----------------------------------------------------
