63b6186667ca6ad65f80a71d0cd448e72ed913e7
rayl3r <39123194+rayl3r@users.noreply.github.com>
2022-03-02 13:01:31 +0100

    Use a logger

--- flappy_bird_NN/main.py
+++ flappy_bird_NN/main.py
@@ -5,7 +5,35 @@ __docformat__ = "google"
 
 import pyglet
 from window import app
+import logging
+from logging import config
+
+log_config = {
+    "version":1,
+    "root":{
+        "handlers" : ["log_handler"],
+        "level": "DEBUG"
+    },
+    "handlers":{
+        "log_handler":{
+            "formatter": "std_out",
+            "class": "logging.FileHandler",
+            "level": "DEBUG",
+            "filename":"app.log"
+        }
+    },
+    "formatters":{
+        "std_out": {
+            "format": "%(asctime)s : %(levelname)s : %(module)s : %(funcName)s : %(lineno)d : %(message)s",
+            "datefmt":"%d-%m-%Y %I:%M:%S"
+        }
+    },
+}
+
+config.dictConfig(log_config)
 
 if __name__ == '__main__':
+    logging.info("Start app")
+
     window = app()
     pyglet.app.run()
